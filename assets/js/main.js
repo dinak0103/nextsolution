973ddb880550f8255a6c4dcaad105522a7d60693
Dina K <dinak010399@gmail.com>
2026-01-19 16:30:40 +0300

    add mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,31 @@
-/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('toggle'),
+      navClose = document.getElementById('nav-close'),
+      overlay = document.getElementById('overlay');
 
+if(navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+        overlay.style.display = 'block';
+    })
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+        overlay.style.display = 'none';
+    })
+}
 
+overlay.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+    overlay.style.display = 'none';
+  });
 
-/*=============== HOME TYPED JS ===============*/
+const navLink = document.querySelectorAll('.menu__link')
 
-
-/*=============== ADD SHADOW HEADER ===============*/
-
-
-/*=============== CONTACT EMAIL JS ===============*/ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
